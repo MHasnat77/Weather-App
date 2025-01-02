4774f2ac0ffa925cc3224220ffe85b692bb264ba
hasnat <hasnat.muhammad1712@gmail.com>
2025-01-02 18:47:01 +0500

    Modified code

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -39,29 +39,32 @@ const Weather = ({ weather, forecast }) => {
         </div>
 
         {/* Weather Details */}
-        <div className="w-[80%] mt-[10vh]">
-          <h4 className="text-[3.5vw] bg-clip-text text-transparent bg-gradient-to-r from-[#87CEEB] to-[#E0FFFF]  w-[20vw] justify-center items-center mx-[30vw] drop-shadow-xl outline-4">
+        <div className="w-[80%] mt-[10vh] ">
+          <h4 className="text-[3.5vw]  text-gray-600   w-[20vw] justify-center items-center mx-[30vw] drop-shadow-xl outline-4">
             {weather.weather?.[0]?.description.toLocaleUpperCase()}
           </h4>
           <h4 className="text-[1.5vw] bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF] to-[#B0C4DE] flex ml-[13vw] mt-[3vw] drop-shadow-lg font-medium">
             {weather.sys?.country}, <Date />
           </h4>
 
-          <div className="flex ml-[13vw] mt-[0.3vw] w-[26.042vw] mb-[0.5vh]">
+          <div className="flex ml-[13vw] mt-[0.3vw] w-[40vw] h-[15vh] mb-[0.5vh] bg-[rgba(0,0,0,0.1)]">
             <p className="text-[3vw] border-r-[0.208vw] px-[0.3vw] mx-[0.35vw] italic">
               {Math.round(weather.main?.temp)}°
             </p>
-            <p className="text-[1.042vw] ml-[0.156vw]">
-              The air pressure is currently at{" "}
-              <strong>{weather.main?.pressure}hPa</strong>, visibility at{" "}
-              <strong>{weather.visibility}m</strong>, and feels like temperature
-              of <strong>{Math.round(weather.main?.feels_like)}°</strong>.
-              Today's temperatures range from{" "}
-              <strong>{Math.round(weather.main?.temp_min)}°</strong> to{" "}
-              <strong>{Math.round(weather.main?.temp_max)}°</strong>.
-            </p>
+            <div className="w-[20vw]">
+              <p className="text-[1.042vw] ml-[0.156vw]">
+                The air pressure is currently at{" "}
+                <strong>{weather.main?.pressure}hPa</strong>, visibility at{" "}
+                <strong>{weather.visibility}m</strong>, and feels like
+                temperature of{" "}
+                <strong>{Math.round(weather.main?.feels_like)}°</strong>.
+                Today's temperatures range from{" "}
+                <strong>{Math.round(weather.main?.temp_min)}°</strong> to{" "}
+                <strong>{Math.round(weather.main?.temp_max)}°</strong>.
+              </p>
+            </div>
           </div>
-          <h4 className="text-[3.5vw] bg-clip-text text-transparent bg-gradient-to-r from-[#87CEEB] to-[#E0FFFF]  w-[40vw] justify-center items-center mx-[30vw] drop-shadow-xl outline-5">
+          <h4 className="text-[3.5vw] text-gray-600  w-[40vw] justify-center items-center mx-[30vw] drop-shadow-xl outline-5">
             Weekly Forecast
           </h4>
           {/* Forecast Deatils */}
